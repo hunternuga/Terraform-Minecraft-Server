648588de98f99c95f3878ff3598a3124e1cdf476
Hunter Nuga <94147854+hunternuga@users.noreply.github.com>
2023-02-06 21:08:52 -0700

    Commiting changes to main.tf
    Changes to main.tf are:
    - adding ubuntu ami lookup only (encountered error where windows ami was being pulled rather than an ubuntu one by default)

--- terraform_minecraft/main.tf
+++ terraform_minecraft/main.tf
@@ -9,6 +9,18 @@ terraform {
 
 data "aws_ami" "ubuntu" {
     most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
 }
 
 resource "aws_security_group" "mc_security_group" {
