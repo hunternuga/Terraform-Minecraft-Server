22acff3d28924ed785dd816073fa794339997a4a
Hunter Nuga <94147854+hunternuga@users.noreply.github.com>
2023-02-08 13:36:56 -0700

    Update variables.tf

--- terraform_minecraft/variables.tf
+++ terraform_minecraft/variables.tf
@@ -6,3 +6,4 @@ variable "public_key" {
 variable "private_key_path" {
   default = "./aws_key"
 }
+#aws_key is here and at working directory since the ssh key should be created in the same working directory of your .tf config files.
