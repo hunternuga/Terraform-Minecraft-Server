4ad9fd966eceb8673ba4dee34e798d9c7c506778
Hunter Nuga <94147854+hunternuga@users.noreply.github.com>
2023-03-05 21:09:10 -0700

    Update main.tf
    Adding more inline commands for server to install on Linux (Ubuntu) server.

--- terraform_minecraft/main.tf
+++ terraform_minecraft/main.tf
@@ -84,9 +84,16 @@ resource "aws_instance" "minecraft_server" {
     inline = [
       "sudo apt-get -y update",
       "sudo apt-get -y install openjdk-8-jre-headless",
+
       "sudo mkdir environment",
       "cd environment",
-      "sudo apt-get install python3"
+      "cd ..",
+
+      "sudo apt install screen",
+      "sudo ufw allow 25565",
+
+      "wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar"
+
     ]
   }
 }
